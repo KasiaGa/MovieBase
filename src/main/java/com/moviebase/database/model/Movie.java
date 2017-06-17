c0272daf0b90b7a1b8eb96dde790284337e3a279
julioVa <damian.trybala@gmail.com>
2017-06-17 13:40:50 +0200

    Adding database dependencies and Movie model class

---
+++ src/main/java/com/moviebase/database/model/Movie.java
@@ -0,0 +1,54 @@
+package com.moviebase.database.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String decription;
+
+    @Column(name = "image")
+    private byte[] image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
