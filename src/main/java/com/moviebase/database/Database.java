1a5ddf80a7787f254bd353764d077732320cb772
julioVa <damian.trybala@gmail.com>
2017-06-22 19:59:26 +0200

    Adding bytea image loading

--- src/main/java/com/moviebase/database/Database.java
+++ src/main/java/com/moviebase/database/Database.java
@@ -3,6 +3,8 @@ package com.moviebase.database;
 import com.moviebase.database.model.Movie;
 import org.hibernate.Session;
 
+import java.io.*;
+
 public class Database {
 
     public static void main(String[] args) {
@@ -10,11 +12,31 @@ public class Database {
         session.beginTransaction();
 
         Movie movie = new Movie();
-        movie.setName("Movie1");
-        movie.setDescription("Description");
+        movie.setName("Title");
+        movie.setDescription("Desc");
+        movie.setImage(getByteaImage("custom path"));
 
         session.save(movie);
         session.getTransaction().commit();
         HibernateUtils.shutdown();
+    }
+
+    public static byte[] getByteaImage(String path){
+
+        try {
+        File file = new File(path);
+
+        FileInputStream fis = new FileInputStream(file);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] buf = new byte[1024];
+
+            for (int readNum; (readNum = fis.read(buf)) != -1;) {
+                bos.write(buf, 0, readNum);
+            }
+            return bos.toByteArray();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
     }
 }
