d7b973633652f2f09b0996521de5a5c6522e62e3
KasiaGa <kasik2121@gmail.com>
2017-06-27 21:21:24 +0200

    Added logout button

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -5,5 +5,7 @@ $(document).ready(function () {
         "<li><a href=\"profile.html\">Profile</a></li>" +
         "<li><a href=\"index.html\">Movies</a></li>" +
         "<li><a href=\"likes.html\">Likes</a></li>" +
-        "<li><a href=\"search.html\">Search</a></li> </ul> </div>")
+        "<li><a href=\"search.html\">Search</a></li> </ul>" +
+        '<a href="welcome.html" class="right hide-on-med-and-down nav-menu-right waves-effect waves-light btn" style="margin-top: 14px; margin-right: 20px;" onclick="signOut();"><i class="material-icons left" style="margin-top: -14px; margin-left:-5px;">power_settings_new</i>logout</a></div>'
+    )
 });
