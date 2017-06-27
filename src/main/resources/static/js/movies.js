d35b3f7b87de35c7fe9f1b886b31766ed1977984
KasiaGa <kasik2121@gmail.com>
2017-06-27 22:02:57 +0200

    Fixes with redirecting

--- src/main/resources/static/js/movies.js
+++ src/main/resources/static/js/movies.js
@@ -1,4 +1,11 @@
-angular.module('moviebase', []).controller('repeatController', ['$scope', '$http', function ($scope, $http) {
+angular.module('moviebase', []).controller('repeatController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+    $http.get('/loggedin').
+        then(function (response) {
+            if(response.data == false) {
+                var host = $window.location.host;
+                $window.location.href = "https://" + host + "/welcome";
+            }
+        });
     $http.get('/movies').
         then(function (response) {
             $scope.movies = response.data;
--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,4 +1,12 @@
-angular.module('moviebase', []).controller('searchController', ['$scope', '$http', function ($scope, $http) {
+angular.module('moviebase', []).controller('searchController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+
+    $http.get('/loggedin').
+        then(function (response) {
+            if(response.data == false) {
+                var host = $window.location.host;
+                $window.location.href = "https://" + host + "/welcome";
+            }
+        });
 
     $scope.SendData = function () {
         var data = {
