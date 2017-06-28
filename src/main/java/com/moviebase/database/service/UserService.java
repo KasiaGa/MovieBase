fe3d1b64f82a5207c18f47cdce3d544e91500131
julioVa <damian.trybala@gmail.com>
2017-06-28 22:03:55 +0200

    Minor bug fixes

--- src/main/java/com/moviebase/database/service/UserService.java
+++ src/main/java/com/moviebase/database/service/UserService.java
@@ -4,11 +4,12 @@ import com.moviebase.database.HibernateUtils;
 import com.moviebase.database.model.User;
 import org.hibernate.Session;
 
+import javax.persistence.NoResultException;
 import java.util.List;
 
 public class UserService {
 
-    public static List<User> getAllUser(){
+    public static List<User> getAllUser() {
         Session session = HibernateUtils.getSession();
         session.beginTransaction();
         List<User> users = session.createQuery("FROM User", User.class).getResultList();
@@ -17,39 +18,47 @@ public class UserService {
         return users;
     }
 
-   public static User getUser(int id){
-       Session session = HibernateUtils.getSession();
-       session.beginTransaction();
-       User user = session.createQuery("FROM User u WHERE u.id = :id", User.class).setParameter("id",id).getSingleResult();
-       return user;
-   }
+    public static User getUser(int id) {
+        try {
+            Session session = HibernateUtils.getSession();
+            session.beginTransaction();
+            User user = session.createQuery("FROM User u WHERE u.id = :id", User.class).setParameter("id", id).getSingleResult();
+            return user;
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
 
-   public static User getUserByEmail(String email){
-       Session session = HibernateUtils.getSession();
-       session.beginTransaction();
-       User user = session.createQuery("FROM User u WHERE u.email = :email", User.class).setParameter("email",email).getSingleResult();
-       return user;
-   }
+    public static User getUserByEmail(String email) {
+        try {
+            Session session = HibernateUtils.getSession();
+            session.beginTransaction();
+            User user = session.createQuery("FROM User u WHERE u.email = :email", User.class).setParameter("email", email).getSingleResult();
+            return user;
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
 
-   public static int save(User user){
-       Session session = HibernateUtils.getSessionFactory().openSession();
-       session.beginTransaction();
-       int id = (int) session.save(user);
-       session.getTransaction().commit();
-       return id;
-   }
+    public static int save(User user) {
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        session.beginTransaction();
+        int id = (int) session.save(user);
+        session.getTransaction().commit();
+        return id;
+    }
 
-   public static void update(User user){
-       Session session = HibernateUtils.getSessionFactory().openSession();
-       session.beginTransaction();
-       session.saveOrUpdate(user);
-       session.getTransaction().commit();
-   }
+    public static void update(User user) {
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.saveOrUpdate(user);
+        session.getTransaction().commit();
+    }
 
-   public static void delete(User user){
-       Session session = HibernateUtils.getSessionFactory().openSession();
-       session.beginTransaction();
-       session.delete(user);
-       session.getTransaction().commit();
-   }
+    public static void delete(User user) {
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(user);
+        session.getTransaction().commit();
+    }
 }
